f6efdf0737b93b3b3b1b8e1bb23794293d74abdf
MonicaMedhat <30467988+MonicaMedhat@users.noreply.github.com>
2019-06-17 03:10:33 +0200

    Delete constants.php

--- constants.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('DB_USER', "root"); // db user
-define('DB_PASSWORD', ""); // db password (mention your db password here)
-define('DB_NAME', "gp"); // database name
-define('DB_HOST', "localhost"); // db server
-?>
